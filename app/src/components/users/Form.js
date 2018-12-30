6ec34de4eab37024b132b7ed967d808531f80c15
vooit <wojciechpac@gmail.com>
2018-12-30 21:08:29 +0100

    finally working datepicker

--- app/src/components/users/Form.js
+++ app/src/components/users/Form.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import TextInput from './TextInput';
-
+import 'react-dates/lib/css/_datepicker.css';
+import {SingleDatePicker} from 'react-dates';
+import 'react-dates/initialize';
+import moment from 'moment';
 
 class Form extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            date: moment(),
+            focused: null
+        }
+    }
+
+    handleDateChange(date) {
+        console.log(date)
+        this.setState({date: date})
     }
 
     render() {
@@ -12,6 +24,28 @@ class Form extends React.Component {
         return (
             <div className="col-12">
                 <form>
+                    <SingleDatePicker
+                        // showClearDate={true}
+                        customInputIcon={
+                            <img src="https://image.flaticon.com/icons/svg/747/747310.svg"
+                                 width="20"
+                                 height="20"
+                                 alt="Calendar free icon"
+                                 title="Calendar"></img>
+                        }
+                        inputIconPosition="after"
+                        small={true}
+                        block={false}
+                        numberOfMonths={1}
+                        date={this.state.date}
+                        onDateChange={date => this.handleDateChange(date)}
+                        focused={this.state.focused}
+                        onFocusChange={({focused}) =>
+                            this.setState({focused})
+                        }
+                        openDirection="up"
+                        hideKeyboardShortcutsPanel={true}
+                    />
                     <TextInput
                         name="firstName"
                         label="First Name"
@@ -29,7 +63,8 @@ class Form extends React.Component {
                         label="Email"
                         value={this.props.user.email}
                         onChange={this.props.onChange}/>
-                    {this.props.emailValid ? <span className="alert alert-danger">Email is required an must contain "@"</span> :  null}
+                    {this.props.emailValid ?
+                        <span className="alert alert-danger">Email is required an must contain "@"</span> : null}
                     <br/>
                     <input
                         type="submit"
@@ -40,7 +75,6 @@ class Form extends React.Component {
 
                 </form>
                 <p>is Valid {this.props.valid}</p>
-
             </div>
         );
     }
